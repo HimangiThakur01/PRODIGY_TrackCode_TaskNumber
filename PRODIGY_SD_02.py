4af6562255e8e00b506d9bb1bc23cab1cec943f8
HimangiThakur01 <144619099+HimangiThakur01@users.noreply.github.com>
2023-09-27 19:10:45 +0530

    Create PRODIGY_SD_02.py

---
+++ PRODIGY_SD_02.py
@@ -0,0 +1,28 @@
+import random
+
+def guessing_game():
+    # Generate a random number between 1 and 100
+    secret_number = random.randint(1, 100)
+
+    attempts = 0
+
+    print("Welcome to the Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    while True:
+        try:
+            user_guess = int(input("Guess the number: "))
+            attempts += 1
+
+            if user_guess < secret_number:
+                print("Too low! Try again.")
+            elif user_guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+if __name__ == "__main__":
+    guessing_game()
